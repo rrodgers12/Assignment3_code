dc2b1d8b19928af38b78ab1c73fa314187f7cbbd
RRodgers12 <rachelrodgers12@gmail.com>
2017-07-11 13:22:07 -0500

    Update to programming 3 assignment scripts.

--- rankhospital.R
+++ rankhospital.R
@@ -1,15 +1,14 @@
-# best.R
-# Finds the hospital with the lowest mortality rate for a given condition in a given state
+# rankhospital
+# Return name of hospital with specified rank for specified condition
 
-best <- function(state, outcome) {
-        # Change cases of state to all caps, outcome to all lower case to ensure conditionals work
+rankhospital <- function(state, outcome, num = "best") {
+        # Change cases of state to all caps, outcome and num to all lower case to ensure conditionals work
         state <- toupper(state)
         outcome <- tolower(outcome)
+        num <- tolower(as.character(num)) # as.character in case num is passed in as an integer
         
         # Read in the data from cwd and extract only the needed columns
         # colClasses = "character" keeps R from "guessing," makes converting to numeric easier
-        # Needed columns:  2 = name, 7 = state,
-        # "heart attack" = col 11, "heart failure" = col 17, "pneumonia" = col 23
         outcomeFullDF <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
         outcomeResizedDF <- outcomeFullDF[ , c(2,7,11,17,23)]
         
@@ -23,20 +22,40 @@ best <- function(state, outcome) {
         # Else, set outcomeColumn based on the value of the outcome argument
         # Default value to the switch runs the stop() function
         outcomeColumn = 0
-
+        
         switch(outcome, 
                "heart attack" = outcomeColumn <- 3,
                "heart failure" = outcomeColumn <- 4,
                "pneumonia" = outcomeColumn <- 5,
                stop("invalid outcome"))
-
+        
         # Subset the full data frame by state and removing entries with missing data in specied condition column 
         stateSubset <- subset(outcomeResizedDF, subset = outcomeResizedDF$State == state & outcomeResizedDF[ , outcomeColumn] != "Not Available")
-        
         # Order by mortality rate, then hospital name
         stateSubset <- stateSubset[order(as.numeric(stateSubset[ , outcomeColumn]), stateSubset$Hospital.Name), ]
         
-        return(as.vector(stateSubset[1, "Hospital.Name"]))
+        # Count the number of hospitals in the stateSubset dataframe, assign result to a variable to use if num == "worst"
+        # and to check if requested rank > number of hospitals for the state
+        numberOfHospitals <- nrow(stateSubset)
+
+        # use num argument to select correct rank
+        if(num == "best") {
+                num <- 1
+        } else if (num == "worst") {
+                num <- numberOfHospitals
+        } else {
+                num <- as.integer(num)
+        }
+        
+        # Check if the requested rank can be returned
+        if(num > numberOfHospitals) {
+                return(NA) 
+        } else {
+                return(stateSubset[num, "Hospital.Name"])
+                }
         
 }
 
+        
+        
+        
